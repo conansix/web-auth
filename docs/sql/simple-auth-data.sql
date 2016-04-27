e38fc8a7123fc9b16616b0077466487b222ab1fb
howardliu-cn <liuxinghao1988@gmail.com>
2016-04-27 16:14:36 +0800

    修改权限基础数据

--- docs/sql/simple-auth-data.sql
+++ docs/sql/simple-auth-data.sql
@@ -18,14 +18,12 @@ INSERT INTO auth.auth_authority (authority_id, authority, authority_desc, enable
 INSERT INTO auth.auth_authority (authority_id, authority, authority_desc, enabled) VALUES (2, 'ROLE_USER', '用户', 1);
 INSERT INTO auth.auth_authority (authority_id, authority, authority_desc, enabled) VALUES (3, 'ROLE_ANONYMOUS', '游客', 1);
 
-INSERT INTO auth.auth_resource (resource_id, resource_name, resource_desc, resource_type, resource, priority, enabled) VALUES (1, 'index', '首页', NULL, '/index.html', 0, 1);
-INSERT INTO auth.auth_resource (resource_id, resource_name, resource_desc, resource_type, resource, priority, enabled) VALUES (2, 'test1', 'test1', NULL, '/test1', 1, 1);
-INSERT INTO auth.auth_resource (resource_id, resource_name, resource_desc, resource_type, resource, priority, enabled) VALUES (3, 'test2', 'test2', NULL, '/test2', 2, 1);
-INSERT INTO auth.auth_resource (resource_id, resource_name, resource_desc, resource_type, resource, priority, enabled) VALUES (4, 'test3', 'test3', NULL, '/test3', 3, 1);
+INSERT INTO auth.auth_resource (resource_id, resource_name, resource_desc, resource_type, resource, priority, enabled) VALUES (1, 'index', '首页', null, '/index.html', 0, 1);
+INSERT INTO auth.auth_resource (resource_id, resource_name, resource_desc, resource_type, resource, priority, enabled) VALUES (2, 'userAuthorities', '用户权限树', null, '/userAuthorities.html', 1, 1);
+INSERT INTO auth.auth_resource (resource_id, resource_name, resource_desc, resource_type, resource, priority, enabled) VALUES (3, '/user/authTree', '用户权限树数据', null, '/user/authTree.json', 2, 1);
 
 INSERT INTO auth.auth_user_group (id, user_id, group_id) VALUES (1, 1, 1);
 INSERT INTO auth.auth_user_group (id, user_id, group_id) VALUES (2, 1, 2);
-INSERT INTO auth.auth_user_group (id, user_id, group_id) VALUES (3, 1, 3);
 
 INSERT INTO auth.auth_group_authority (id, group_id, authority_id) VALUES (1, 1, 1);
 INSERT INTO auth.auth_group_authority (id, group_id, authority_id) VALUES (2, 1, 2);
@@ -37,11 +35,9 @@ INSERT INTO auth.auth_group_authority (id, group_id, authority_id) VALUES (6, 3,
 INSERT INTO auth.auth_authority_resource (id, authority_id, resource_id) VALUES (1, 1, 1);
 INSERT INTO auth.auth_authority_resource (id, authority_id, resource_id) VALUES (2, 1, 2);
 INSERT INTO auth.auth_authority_resource (id, authority_id, resource_id) VALUES (3, 1, 3);
-INSERT INTO auth.auth_authority_resource (id, authority_id, resource_id) VALUES (4, 1, 4);
-INSERT INTO auth.auth_authority_resource (id, authority_id, resource_id) VALUES (5, 2, 1);
-INSERT INTO auth.auth_authority_resource (id, authority_id, resource_id) VALUES (6, 2, 2);
-INSERT INTO auth.auth_authority_resource (id, authority_id, resource_id) VALUES (7, 2, 3);
-INSERT INTO auth.auth_authority_resource (id, authority_id, resource_id) VALUES (8, 3, 1);
-INSERT INTO auth.auth_authority_resource (id, authority_id, resource_id) VALUES (9, 3, 2);
+INSERT INTO auth.auth_authority_resource (id, authority_id, resource_id) VALUES (4, 2, 1);
+INSERT INTO auth.auth_authority_resource (id, authority_id, resource_id) VALUES (5, 2, 2);
+INSERT INTO auth.auth_authority_resource (id, authority_id, resource_id) VALUES (6, 2, 3);
+INSERT INTO auth.auth_authority_resource (id, authority_id, resource_id) VALUES (7, 3, 1);
 
 SET UNIQUE_CHECKS = 1;
